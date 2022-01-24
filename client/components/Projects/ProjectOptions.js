243426528524849ce05029e0f0b7e64240ce19fc
Greg Solomon <ghsolomon@gmail.com>
2022-01-23 19:59:46 -0500

    feat: enhance projects list

---
+++ client/components/Projects/ProjectOptions.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import {
+  IconButton,
+  Menu,
+  MenuItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+const ProjectOptions = ({ handleDelete }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = !!anchorEl;
+
+  return (
+    <>
+      <IconButton edge="end" onClick={handleClick}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu open={open} anchorEl={anchorEl} onClose={handleClose}>
+        <MenuItem>
+          <ListItemIcon>
+            <ContentCopyIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText>Duplicate</ListItemText>
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            handleClose();
+            handleDelete();
+          }}
+        >
+          <ListItemIcon>
+            <DeleteIcon fontSize="small" color="error" />
+          </ListItemIcon>
+          <ListItemText>Delete</ListItemText>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default ProjectOptions;
--- client/components/Projects/Projects.js
+++ client/components/Projects/Projects.js
@@ -1,20 +1,43 @@
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+  Stack,
+  Typography,
+} from '@mui/material';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchProject, fetchProjects } from '../../store';
+import ProjectOptions from './ProjectOptions';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 
 const Projects = ({ projects, fetchProjects, fetchProject }) => {
   useEffect(() => fetchProjects(), []);
   return (
-    <ul>
-      {projects.map((project) => (
-        <li key={project.id}>
-          <button onClick={() => fetchProject(project.id)}>
-            {project.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Container>
+      <Stack sx={{ margin: 1 }} spacing={1}>
+        <Typography variant="h3">Projects</Typography>
+        <Paper>
+          <List>
+            {projects.map((project) => (
+              <ListItem key={project.id} secondaryAction={<ProjectOptions />}>
+                <ListItemButton onClick={() => fetchProject(project.id)}>
+                  <ListItemIcon>
+                    <TheaterComedyIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={project.name} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Stack>
+    </Container>
   );
 };
 
